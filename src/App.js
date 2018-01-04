86da835006003acf11a9f891d2d9febabd4cf0fd
Freddie Ridell <freddie@codogo.io>
2018-01-04 15:07:50 +0000

    feat(server): server details adding modal, but it is not good

--- src/App.js
+++ src/App.js
@@ -1,1 +1,55 @@
-export default () => <div>codogo-react-app</div>;
+import autobind from 'autobind-decorator';
+
+import {
+	Input,
+	Modal,
+} from "./components/toolbox";
+
+export default class Root extends React.Component {
+	constructor(props){
+		super(props);
+
+	}
+
+	@autobind
+	hasServerInfo(){
+		return localStorage.serverAddress && localStorage.serverKey;
+	}
+
+	@autobind
+	onAddressChange({ target: { value, } }){
+		localStorage.serverAddress = value;
+	}
+
+	@autobind
+	onServerKeyChange({ target: { value, } }){
+		localStorage.serverKey = value;
+	}
+
+	@autobind
+	renderServerInfo(){
+		return (
+			<Modal>
+				address: <Input
+					type = "text"
+					value = { localStorage.serverAddress }
+					onChange = { this.onAddressChange } 
+				/>
+				key: <Input
+					type = "text"
+					value = { localStorage.serverKey }
+					onChange = { this.onServerKeyChange }
+				/>
+			</Modal>
+		);
+	}
+
+	render(){
+		console.log(this.state);
+		return (
+			this.hasServerInfo()
+			? <div />
+			: this.renderServerInfo()
+		);
+	}
+};
---
+++ src/components/toolbox/index.js
@@ -0,0 +1,35 @@
+import styled, { injectGlobal } from "styled-components";
+injectGlobal`
+	html {
+		font-size: 8px;
+	}
+
+	body {
+		font-size: 2rem;
+	}
+
+	html, body, #root {
+		margin: 0;
+		min-height: 100%;
+		height: 100%;
+	}
+
+	#root {
+		display: flex;
+		flex-direction: column;
+	}
+`;
+
+export const Input = styled.input`
+
+`;
+
+export const Modal = styled.div`
+	align-items: center;
+	box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	justify-content: center;
+	margin: 2rem;
+`;
